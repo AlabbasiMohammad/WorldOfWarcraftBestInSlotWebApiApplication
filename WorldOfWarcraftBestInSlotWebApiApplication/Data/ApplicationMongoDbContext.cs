1fecbae3e61a213b0c9686c797c76caa79b0e463
Mohammad Al-Abbasi <mohammad.al-abbasi@student.tugraz.at>
2025-04-08 15:31:34 +0200

    setting the mongodb service

--- WorldOfWarcraftBestInSlotWebApiApplication/Data/ApplicationMongoDbContext.cs
+++ WorldOfWarcraftBestInSlotWebApiApplication/Data/ApplicationMongoDbContext.cs
@@ -1,6 +1,31 @@
-﻿namespace WorldOfWarcraftBestInSlotWebApiApplication.Data
+﻿using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using WorldOfWarcraftBestInSlotWebApiApplication.Models;
+
+namespace WorldOfWarcraftBestInSlotWebApiApplication.Data
 {
     public class ApplicationMongoDbContext
     {
+        private readonly IMongoDatabase _database;
+        public ApplicationMongoDbContext(IOptions<MongoDbSettings> settings)
+        {
+            var client = new MongoClient(settings.Value.ConnectionString);
+            _database = client.GetDatabase(settings.Value.DatabaseName);
+        }
+
+        public IMongoCollection<Item> Items => _database.GetCollection<Item>("Items");
+        public IMongoCollection<Character> Characters => _database.GetCollection<Character>("Characters");
+        public IMongoCollection<Monster> Monsters => _database.GetCollection<Monster>("Monsters");
+
     }
+
+
+
+    public class MongoDbSettings
+    {
+        public string ConnectionString { get; set; } = string.Empty;
+        public string DatabaseName { get; set; } = string.Empty;
+    }
+
+    
 }
